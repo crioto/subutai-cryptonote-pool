34b7a1286a8948ec32a60125cd75e8ae6c1e9c00
Mike Savochkin <crioto@users.noreply.github.com>
2019-03-18 23:20:44 +0600

    Initial setup

---
+++ templates/config.js
@@ -0,0 +1,27 @@
+/* Must point to the API setup in your config.json file. */
+var api = "http://poolhost:8117";
+
+/* Minimum units in a single coin, for Bytecoin its 100000000. */
+var coinUnits = 1000000000000;
+
+/* Pool server host to instruct your miners to point to.  */
+var poolHost = "cryppit.com";
+
+/* IRC Server and room used for embedded KiwiIRC chat. */
+var irc = "irc.freenode.net/#monero";
+
+/* Contact email address. */
+var email = "support@cryppit.com";
+
+/* Market stat display params from https://www.cryptonator.com/widget */
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
+
+/* Download link to cryptonote-easy-miner for Windows users. */
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+/* Used for front-end block links. For other coins it can be changed, for example with
+   Bytecoin you can use "https://minergate.com/blockchain/bcn/block/". */
+var blockchainExplorer = "http://monerochain.info/block/";
+
+/* Used by front-end transaction links. Change for other coins. */
+var transactionExplorer = "http://monerochain.info/tx/";
